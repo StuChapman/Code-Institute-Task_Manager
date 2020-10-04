f2ea046cbb017147c69c1e7993251af1afaf34c1
Stuart Chapman <chapman.stuart@sky.com>
2020-10-04 15:59:32 +0000

    Added addtask.html functionality

--- app.py
+++ app.py
@@ -10,10 +10,24 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0.5zsef.mongodb.ne
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
